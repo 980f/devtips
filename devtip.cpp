06a4fd3e9c77bebd47d4c1343ceebd279b135ef4
andyh <andy.980f@gmail.com>
2020-11-25 11:49:23 -0600

    about to switch to F407 to test with BMP as debugger hardware

--- devtip.cpp
+++ devtip.cpp
@@ -2,20 +2,18 @@
 
 #include "timer.h"
 
-Timer tim{1};
+Timer tim{1};//timer 1 has the full set of features.
 
 
 #include "core_cmInstr.h"
 
 #include "pinconfigurator.h"
-
-MakeObject(PinConfigurator,timeroutput,'E',5,PinOptions::function,PinOptions::slow, PinOptions::F, 3);
-MakeObject(PinConfigurator,uarttx,'A',3,PinOptions::function,PinOptions::slow, PinOptions::F, 3);
-ConfPin(A,4,PinOptions::function,PinOptions::slow, PinOptions::F, 3);
-
+#if 1
+ConfPin(E,5,PinOptions::function,PinOptions::slow, PinOptions::F, 3);
+ConfPin(A,3,PinOptions::function,PinOptions::fast, PinOptions::F, 4);
+ConfPin(A,4,PinOptions::function,PinOptions::fastest, PinOptions::F, 5);
 ConfPin(A,6,PinOptions::input,PinOptions::slow, PinOptions::U, 0);
-
-
+#endif
 
 #pragma ide diagnostic ignored "EndlessLoop"
 int main() {
