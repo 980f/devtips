71de79ce81a24b1761b0109bfad06d170c831e0f
andyh <andy.980f@gmail.com>
2020-11-26 23:46:51 -0600

    configure 4 pwms, and a pin that I can get the LA104 to trigger on to test its setup

--- devtip.cpp
+++ devtip.cpp
@@ -1,11 +1,9 @@
 #include <cortexm/systick.h>
-#include "timer.h"
-const Timer tim{1};//timer 1 has the full set of features.
 
 
 #include "core_cmInstr.h"
-
 #include "pinconfigurator.h"
+
 #if 1
 
 #define DeclSwitch(bitnum) ConfPin(E,bitnum,PinOptions::input,PinOptions::slow, PinOptions::U, 0);\
@@ -14,7 +12,6 @@ static const LogicalPin sw##bitnum({PE,bitnum},false)
 #define DeclLed(bitnum) ConfPin(E,bitnum,PinOptions::output,PinOptions::slow, PinOptions::F, 0);\
 static const LogicalPin led##bitnum({PE,bitnum},false)
 
-
 DeclSwitch(10);
 DeclSwitch(11);
 DeclSwitch(12);
@@ -22,13 +19,26 @@ DeclSwitch(12);
 DeclLed(13);
 DeclLed(14);
 DeclLed(15);
-
 #endif
 
 #include "fpu.h"
 InitStep(InitCore)
 const FpuOptions useFpu(true,false); //loading a floating point value without having turned on the FPU causes an infinite vector through our "generate reset on fault" logic.
 
+#include "timer.h"
+const Timer tim{4};//timer 4 has all 4 channels on handy pins
+
+const CCUnit ch[4]={{tim,1},{tim,2},{tim,3},{tim,4}};
+//PD 12..15 af3
+#define CHPIN(port,bitnum) ConfPin(port,bitnum,PinOptions::function,PinOptions::slow, PinOptions::F, 3);
+CHPIN(D,12)
+CHPIN(D,13)
+CHPIN(D,14)
+CHPIN(D,15)
+
+ConfPin(D,10,PinOptions::output,PinOptions::slow, PinOptions::F, 0);
+
+const LogicalPin D10{{PD,10}};
 
 #pragma ide diagnostic ignored "EndlessLoop"
 int main() {
@@ -38,10 +48,23 @@ int main() {
   tim.init();
   tim.setPrescaleFor(10000);
 
+  tim.setCycler(600);
+
+
+  u8 mode=0b1'110'0'0'00;//mode pwm
+  for(unsigned chi=4; chi-->0;){
+    ch[chi].setTicks(100+20*chi);
+    ch[chi].setmode(mode);
+    ch[chi].takePin(true);
+  } 
+
+  tim.beRunning();
+
   while(true){
     MNE(WFE);
     led13=sw10;
-    led14=sw11;
+    D10=led14=sw11;
     led15=sw12;
+    
   }
 }
